ad6c8cfda4aedbdafb6f5aee2409a8cf8f6fbafe
Clay Johnson <cjohnson@gradle.com>
2024-02-15 16:03:20 -0600

    Enable build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,27 @@
 plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("com.gradle.enterprise") version "3.16.2"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.12.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "new-asf-builds-query"
+
+gradleEnterprise {
+    buildScan {
+        val acceptTOSProp = "acceptGradleTOS"
+        if (extra.properties.sets(acceptTOSProp)) {
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+        publishAlways()
+
+        obfuscation {
+            ipAddresses { addresses -> addresses.map { _ -> "0.0.0.0" } }
+        }
+    }
+}
+
+fun Map<String, Any>.sets(key: String) : Boolean {
+    val value = this.getOrDefault(key, "false").toString()
+    return value.isBlank() || value.toBoolean()
+}
