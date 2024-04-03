09b6c82ee9c245eafd3fa5e8223fad6ef96e7695
Clay Johnson <cjohnson@gradle.com>
2024-04-03 07:39:46 -0500

    Use Develocity plugin over Gradle Enterprise plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-    id("com.gradle.enterprise") version "3.17"
+    id("com.gradle.develocity") version "3.17"
     id("com.gradle.common-custom-user-data-gradle-plugin") version "1.13"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "new-builds-query"
 
-gradleEnterprise {
+develocity {
     buildScan {
         val acceptTOSProp = "acceptGradleTOS"
         if (extra.properties.sets(acceptTOSProp)) {
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
+            termsOfUseUrl = "https://gradle.com/terms-of-service"
+            termsOfUseAgree = "yes"
         }
-        publishAlways()
+        publishing.onlyIf { true }
 
         obfuscation {
             ipAddresses { addresses -> addresses.map { _ -> "0.0.0.0" } }
